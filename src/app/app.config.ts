f9c936837e546bb7cd1995621e6d172ae0d6a588
Massimiliano Gasaro <massimiliano.gasaro@atomoshyla.com>
2024-07-19 11:47:24 +0200

    avanzamento dashboard

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,6 +7,7 @@ import { loaderInterceptor } from './interceptor/loader.interceptor';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideToastr } from 'ngx-toastr';
+import { provideNativeDateAdapter } from '@angular/material/core';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -19,6 +20,7 @@ export const appConfig: ApplicationConfig = {
       positionClass: 'toast-top-right',
       preventDuplicates: true,
     }),
+    provideNativeDateAdapter(),
     provideAnimationsAsync(),
     { provide: HTTP_INTERCEPTORS, useValue: loaderInterceptor, multi: true }, provideAnimationsAsync(),
   ]
--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -3,8 +3,9 @@ import { TableComponent } from '../../shared/table/table.component';
 import { Expense } from '../../interfaces/expenses';
 import { HttpApiService } from '../../services/httpApi.service';
 import { MatIconModule } from '@angular/material/icon';
-import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { MatDialog, MatDialogConfig, MatDialogModule } from '@angular/material/dialog';
 import { CustomDialogComponent } from '../../shared/custom-dialog/custom-dialog.component';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-dashboard',
@@ -14,7 +15,9 @@ import { CustomDialogComponent } from '../../shared/custom-dialog/custom-dialog.
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent implements OnInit {
-  constructor(private apiService: HttpApiService, private dialog: MatDialog)
+  constructor(private apiService: HttpApiService, private dialog: MatDialog,
+    private toastr: ToastrService
+  )
   {}
 
   public expensesList: Array<Expense> = [];
@@ -22,37 +25,67 @@ export class DashboardComponent implements OnInit {
   ngOnInit(): void {
     this.expensesList =  [
       {
-      id: 1,
-      name: "GELATO",
-      where: "torino",
-      cost: 3,
-      type: "svago",
-      currency: "euro",
-      when: ""
-    },
-    {
-      id: 2,
-      name: "GELATO",
-      where: "torino",
-      cost: 3,
-      type: "svago",
-      currency: "euro",
-      when: ""
-    }
-  ];
+        id: 1,
+        name: "GELATO",
+        where: "torino",
+        cost: 3,
+        type: "svago",
+        currency: "euro",
+        when: ""
+      },
+      {
+        id: 2,
+        name: "GELATO",
+        where: "torino",
+        cost: 3,
+        type: "svago",
+        currency: "euro",
+        when: ""
+      }
+    ];
   }
 
   public onAddRecord() {
-    console.log("ADD");
-    this.dialog.open(CustomDialogComponent);
+    let config: MatDialogConfig = {
+      width: "30%",
+      data: {
+        action: "insert-record"
+      }
+    }
+
+    this.openDialog(config);
   }
 
-  public onUpdateRecord() {
-    console.log("UPDATE");
+  public onUpdateRecord(id: number) {
+    const itemUpdate = this.expensesList.filter(v => v.id === id);
+    let config: MatDialogConfig = {
+      width: "30%",
+      data: {
+        action: "update-record",
+        id: id,
+        name: itemUpdate[0].name,
+        where: itemUpdate[0].where,
+        cost: itemUpdate[0].cost,
+        type: itemUpdate[0].type,
+        date: itemUpdate[0].when,
+        currency: itemUpdate[0].currency
+      }
+    }
+
+    this.openDialog(config);
   }
 
-  public onRemoveRecord() {
-    console.log("REMOVE");
+  public onRemoveRecord(id: number) {
+    const itemDelete = this.expensesList.filter(v => v.id === id);
+    let config: MatDialogConfig = {
+      width: "20%",
+      data: {
+        action: "remove-record",
+        id: id,
+        name: itemDelete[0].name,
+      }
+    }
+    this.openDialog(config);
   }
 
   // PRIVATE METHODS
@@ -63,4 +96,27 @@ export class DashboardComponent implements OnInit {
       error: err => console.error(err)
     })
   }
+
+  private openDialog(config: MatDialogConfig): void {
+    const dialogRef = this.dialog.open(CustomDialogComponent, config);
+
+    dialogRef.afterClosed().subscribe({
+      next: result => {
+        console.log('The dialog was closed');
+        console.log(result);
+      },
+      error: err => console.error(err)
+    });
+  }
+}
+
+export interface DialogData {
+  action: string;
+  name: string;
+  where: string;
+  cost: number;
+  type: string;
+  date: string;
+  currency: string;
+  id: number;
 }
